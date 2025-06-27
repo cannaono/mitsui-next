878f474707e0d29edb5d44ce01456fc4603653f8
k.ono <k.ono@newtonit.com>
2025-06-27 09:17:32 +0100

    for production build

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,11 @@ const userAuth = Buffer.from(process.env.USER_AUTH || '').toString('base64')
 const adminAuth = Buffer.from(process.env.ADMIN_AUTH || '').toString('base64')
 
 export function middleware(request: NextRequest) {
+	// Skip if dev
+	if (process.env.NODE_ENV === 'development') {
+		return NextResponse.next();
+	}
+	// Basic authentication depending on the page
 	const url = request.nextUrl
 	const path = url.pathname
 	const authHeader = request.headers.get('authorization')
@@ -19,7 +24,6 @@ export function middleware(request: NextRequest) {
 			'WWW-Authenticate': 'Basic realm="Secure Area"',
 		},
 	})
-	return NextResponse.next()
 }
 
 // Set the paths for authorisation
